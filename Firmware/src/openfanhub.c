07502a5a87e97a06077810ac39022ac1b56258b9
Kevin <smeatsan@gmail.com>
2021-12-26 18:42:58 +0100

    Prepare USB rx

--- Firmware/src/openfanhub.c
+++ Firmware/src/openfanhub.c
@@ -1,4 +1,3 @@
-
 #include "stm32f1xx.h"
 #include "STM32F1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h"
 #include <stm32f1xx_hal.h>
@@ -13,21 +12,21 @@
 #include "xprintf.h"
 
 
-extern int8_t USBD_CUSTOM_HID_SendReport_FS ( uint8_t *report,uint16_t len);
-
-
-
-static uint8_t* USBD_HID_GetPos (void) {
-	static int8_t HID_Buffer[9] = {0};
-	return (uint8_t*)HID_Buffer;
-}
+//extern int8_t USBD_CUSTOM_HID_SendReport_FS ( uint8_t *report,uint16_t len);
+//extern PCD_HandleTypeDef hpcd_USB_FS;
 
 #define LED_PORT                GPIOC
 #define LED_PIN                 GPIO_PIN_13
- #define LED_PORT_CLK_ENABLE     __HAL_RCC_GPIOC_CLK_ENABLE
+#define LED_PORT_CLK_ENABLE     __HAL_RCC_GPIOC_CLK_ENABLE
 
 
+//USBD_StatusTypeDef USBD_LL_Transmit(USBD_HandleTypeDef *pdev, uint8_t ep_addr, uint8_t *pbuf, uint16_t size)
 
+void on_usb_rx(void* data) {
+	//uint8_t response[16] = {0};
+	//USBD_CUSTOM_HID_SendReport_FS(response, 16);
+	__WFI();
+}
 
 int main() {
 	init_cubemx();
