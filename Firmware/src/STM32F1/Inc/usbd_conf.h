2050c6703dedae50e94482cc66e463a9b311e721
Kevin <smeatsan@gmail.com>
2021-12-26 21:34:29 +0100

    Fix problems with corrupted data

--- Firmware/src/STM32F1/Inc/usbd_conf.h
+++ Firmware/src/STM32F1/Inc/usbd_conf.h
@@ -74,7 +74,7 @@
 /*---------- -----------*/
 #define USBD_CUSTOMHID_OUTREPORT_BUF_SIZE     16
 /*---------- -----------*/
-#define USBD_CUSTOM_HID_REPORT_DESC_SIZE     39
+#define USBD_CUSTOM_HID_REPORT_DESC_SIZE     35
 /*---------- -----------*/
 #define CUSTOM_HID_FS_BINTERVAL     0x5
 
