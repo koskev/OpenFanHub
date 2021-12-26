04b87e0c2971feaf9230f4901396eb027c20b4ee
Kevin <smeatsan@gmail.com>
2021-12-26 16:05:19 +0100

    Initial commit

---
+++ Firmware/src/build/CMakeFiles/openfanhub.dir/home/kevin/STM32Cube/Repository/STM32Cube_FW_F1_V1.8.4/Drivers/CMSIS/Device/ST/STM32F1xx/Source/Templates/system_stm32f1xx.c.obj.d
@@ -0,0 +1,16 @@
+CMakeFiles/openfanhub.dir/home/kevin/STM32Cube/Repository/STM32Cube_FW_F1_V1.8.4/Drivers/CMSIS/Device/ST/STM32F1xx/Source/Templates/system_stm32f1xx.c.obj: \
+ /home/kevin/STM32Cube/Repository/STM32Cube_FW_F1_V1.8.4/Drivers/CMSIS/Device/ST/STM32F1xx/Source/Templates/system_stm32f1xx.c \
+ /home/kevin/STM32Cube/Repository/STM32Cube_FW_F1_V1.8.4/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h \
+ /home/kevin/STM32Cube/Repository/STM32Cube_FW_F1_V1.8.4/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ /home/kevin/STM32Cube/Repository/STM32Cube_FW_F1_V1.8.4/Drivers/CMSIS/Include/core_cm3.h \
+ /usr/lib/gcc/arm-none-eabi/11.2.0/include/stdint.h \
+ /usr/arm-none-eabi/include/stdint.h \
+ /usr/arm-none-eabi/include/machine/_default_types.h \
+ /usr/arm-none-eabi/include/sys/features.h \
+ /usr/arm-none-eabi/include/_newlib_version.h \
+ /usr/arm-none-eabi/include/sys/_intsup.h \
+ /usr/arm-none-eabi/include/sys/_stdint.h \
+ /home/kevin/STM32Cube/Repository/STM32Cube_FW_F1_V1.8.4/Drivers/CMSIS/Include/cmsis_version.h \
+ /home/kevin/STM32Cube/Repository/STM32Cube_FW_F1_V1.8.4/Drivers/CMSIS/Include/cmsis_compiler.h \
+ /home/kevin/STM32Cube/Repository/STM32Cube_FW_F1_V1.8.4/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /home/kevin/STM32Cube/Repository/STM32Cube_FW_F1_V1.8.4/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h
---
+++ Firmware/src/build/CMakeFiles/openfanhub.dir/xprintf.c.obj.d
@@ -0,0 +1,4 @@
+CMakeFiles/openfanhub.dir/xprintf.c.obj: \
+ /mnt/hdd/home/kevin/Dokumente/Projekte/Github/OpenFanHub/Firmware/src/xprintf.c \
+ /mnt/hdd/home/kevin/Dokumente/Projekte/Github/OpenFanHub/Firmware/src/xprintf.h \
+ /usr/lib/gcc/arm-none-eabi/11.2.0/include/stdarg.h
