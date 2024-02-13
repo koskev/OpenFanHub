f82e937c3a6c0db200ee1ef65fe0018008e82c65
Kevin <kevin@kokev.de>
2024-02-13 13:25:27 +0100

    Add timer etc

--- Firmware/src/openfanhub.c
+++ Firmware/src/openfanhub.c
@@ -11,7 +11,31 @@
 #include <stdint.h>
 #include <string.h>
 
+/**
+ *
+ *  TODO:
+ *  	Actual PWM value differs from requested one, skipping. Requested: 1 Actual: 0
+  DEBUG   Setting Fan PWM of 'hub_fan4' to 2 ...
+  DEBUG   Actual PWM value differs from requested one, skipping. Requested: 2 Actual: 0
+ */
+
+/**
+ * Notes Fan:
+ * PWM
+ * https://www.glkinst.com/cables/cable_pics/4_Wire_PWM_Spec.pdf
+ * https://noctua.at/pub/media/wysiwyg/Noctua_PWM_specifications_white_paper.pdf
+ * 	25kHz, acceptable range 21kHz to 28kHz
+ * 	max: 5,25V 
+ * 	low: 0.8V
+ * 		3.3V PWM should be fine
+ *
+ *
+ */
+
 extern int8_t USBD_CUSTOM_HID_SendReport_FS ( uint8_t *report,uint16_t len);
+extern TIM_HandleTypeDef htim2;
+extern TIM_HandleTypeDef htim3;
+extern TIM_HandleTypeDef htim4;
 
 enum CONTROL {
 	CTL_GET_TMP_CNCT = 0x10,
@@ -19,9 +43,16 @@ enum CONTROL {
 	CTL_GET_FAN_CNCT = 0x20,
 	CTL_GET_FAN_RPM = 0x21,
 	CTL_GET_FAN_PWM = 0x22,
-	CTL_SET_FAN_RPM = 0x23,
+	CTL_SET_FAN_FPWM = 0x23,
 	CTL_SET_FAN_TARGET = 0x24
 };
+
+
+#define DIV_ROUND(dividend, divisor) (\
+		((dividend - 1) > 0 || dividend > 0) ? \
+		((dividend + (divisor / 2)) / divisor) : \
+		((dividend - (divisor / 2)) / divisor) \
+		)
 
 
 
@@ -29,14 +60,41 @@ enum CONTROL {
 #define LED_PIN                 GPIO_PIN_13
 #define LED_PORT_CLK_ENABLE     __HAL_RCC_GPIOC_CLK_ENABLE
 #define RESPONSE_SIZE 16
+#define NUM_FANS 6 // limited by the driver
+#define NUM_TEMP_SENSORS 4
 
+
+#define MAX_PWM 65534
+#define MAX_RPM 3000
+
+typedef struct fan_handle_s {
+	uint8_t pwm_percent;
+	TIM_HandleTypeDef* pwm_handle;
+	uint32_t pwm_channel;
+} fan_handle_t;
+
+static fan_handle_t fans[NUM_FANS];
 
 int get_fan_type(int fan_id) {
 	return 0x02;
 }
 
 uint16_t get_fan_rpm(int fan_id) {
-	return 1500;
+	return (fans[fan_id].pwm_percent/100.0f) * MAX_RPM;
+}
+
+uint16_t get_fan_pwm(int fan_id) {
+	return fans[fan_id].pwm_percent;
+}
+
+
+void set_fan_pwm_percentage(uint8_t fan_id, uint8_t pwm_percent) {
+	uint16_t pwm = MAX_PWM * (pwm_percent/100.0f) + 0.5;
+	//current_pwm = DIV_ROUND(MAX_PWM * pwm_percent, 100);
+	TIM_HandleTypeDef* handle = fans[fan_id].pwm_handle;
+	if(handle) {
+		__HAL_TIM_SET_COMPARE(handle, fans[fan_id].pwm_channel, pwm);
+	}
 }
 
 uint16_t get_temp(int temp_id) {
@@ -52,12 +110,12 @@ void on_usb_rx(void* data) {
 	switch(command_id) {
 		case CTL_GET_TMP_CNCT:
 			// fill byte 1-4 with 1 if a temp probe is present
-			for(int i = 0; i < 4; ++i) {
+			for(int i = 0; i < NUM_TEMP_SENSORS; ++i) {
 				response[i+1] = 0x01;
 			}
 			break;
 		case CTL_GET_FAN_CNCT:
-			for(int i = 0; i < 6; ++i) {
+			for(int i = 0; i < NUM_FANS; ++i) {
 				response[i+1] = get_fan_type(i);
 			}
 			break;
@@ -67,6 +125,20 @@ void on_usb_rx(void* data) {
 				response[1] = rpm >> 8;
 				response[2] = rpm;
 
+			}
+			break;
+		case CTL_GET_FAN_PWM:
+			{
+				uint16_t pwm = get_fan_pwm(data_8[1]);
+				response[1] = pwm;
+				//response[1] = (pwm*100.0f) / MAX_PWM + 0.5f;
+			}
+			break;
+		case CTL_SET_FAN_FPWM:
+			{
+				uint8_t fan_num = data_8[1];
+				uint8_t percentage = data_8[2];
+				set_fan_pwm_percentage(fan_num, percentage);
 			}
 			break;
 		case CTL_GET_TMP:
@@ -80,11 +152,36 @@ void on_usb_rx(void* data) {
 			break;
 	};
 	USBD_CUSTOM_HID_SendReport_FS(response, 16);
-	//HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+	HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+}
+
+void init_fan(int id, TIM_HandleTypeDef* handle, uint32_t channel) {
+	HAL_TIM_Base_Start_IT(handle);
+	HAL_TIM_PWM_Start(handle, channel);
+	__HAL_TIM_SET_COMPARE(handle, channel, 0);
+
+	fans[id].pwm_handle = handle;
+	fans[id].pwm_channel = channel;
+}
+
+void init_fans() {
+	init_fan(0, &htim2, TIM_CHANNEL_1);
+	init_fan(1, &htim2, TIM_CHANNEL_3);
+
+	init_fan(2, &htim3, TIM_CHANNEL_1);
+	init_fan(3, &htim3, TIM_CHANNEL_3);
+
+	init_fan(4, &htim4, TIM_CHANNEL_1);
+	init_fan(5, &htim4, TIM_CHANNEL_3);
 }
 
 int main() {
+	memset(fans, 0, sizeof(fan_handle_t) * NUM_FANS);
 	init_cubemx();
+	
+	init_fans();
+	
+	set_fan_pwm_percentage(0, 50);
 
 
 	while(42) {
