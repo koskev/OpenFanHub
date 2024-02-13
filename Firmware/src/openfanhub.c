87d8e61f5e29d779aaff6e1dfdf3f36ac7e2a403
Kevin <kevin@kokev.de>
2024-02-13 13:25:27 +0100

    Copy init code to openfanhub as cubemx messes up the dma adc init order

--- Firmware/src/openfanhub.c
+++ Firmware/src/openfanhub.c
@@ -7,6 +7,13 @@
 #include <stm32f1xx_hal.h>
 #include <stm32f1xx_hal_conf.h>
 #include <main.h>
+#include <adc.h>
+#include <dma.h>
+#include <i2c.h>
+#include <tim.h>
+#include <usart.h>
+#include <usb_device.h>
+#include <gpio.h>
 
 #include "STM32F1/Inc/usbd_custom_hid_if.h"
 #include "stm32f1xx_hal_adc.h"
@@ -309,11 +316,69 @@ void read_adc() {
 	HAL_ADC_Start_DMA(&hadc1, (uint32_t*)&temps, 4);
 }
 
+void init_system_clock(void) {
+	RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+	RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+	
+	/** Initializes the RCC Oscillators according to the specified parameters
+	* in the RCC_OscInitTypeDef structure.
+	*/
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+	RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+	RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+	{
+	  Error_Handler();
+	}
+	/** Initializes the CPU, AHB and APB buses clocks
+	*/
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+	                            |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+	
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+	{
+	  Error_Handler();
+	}
+	PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_ADC|RCC_PERIPHCLK_USB;
+	PeriphClkInit.AdcClockSelection = RCC_ADCPCLK2_DIV6;
+	PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_PLL_DIV1_5;
+	if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+	{
+	  Error_Handler();
+	}
+}
+
+void init() {
+	HAL_Init();
+
+	init_system_clock();
+	MX_GPIO_Init();
+	MX_TIM1_Init();
+	MX_TIM2_Init();
+	MX_TIM3_Init();
+	MX_TIM4_Init();
+	MX_USB_DEVICE_Init();
+	MX_DMA_Init();
+	MX_ADC1_Init();
+	MX_I2C2_Init();
+	MX_USART2_UART_Init();
+	
+}
+
 int main() {
 	for(int i = 0; i < sizeof(fan_handle_t) * NUM_FANS; ++i) {
 		*((volatile uint8_t*)fans+i) = 0;
 	}
-	init_cubemx();
+	init();
 	HAL_ADCEx_Calibration_Start(&hadc1);
 	HAL_ADC_Start_DMA(&hadc1, (uint32_t*)&temps, 4);
 	
