af1d662061ebdce0d7e01c0d8296d47b67cf383b
Kevin <kevin@kokev.de>
2024-02-13 13:25:27 +0100

    Move more functions in fan file

--- Firmware/src/fan.c
+++ Firmware/src/fan.c
@@ -3,6 +3,55 @@
 
 #include "fan.h"
 
+/**
+ * Returns 1 if a 4-pin fan is detected. 0 otherwise
+ */
+static int fan_detect_4pin(GPIO_TypeDef* pwm_port, uint16_t pwm_pin) {
+	int ret = 0;
+	// enable input
+	GPIO_InitTypeDef GPIO_InitStruct = {0};
+	GPIO_InitStruct.Pin = pwm_pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+	GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	HAL_GPIO_Init(pwm_port, &GPIO_InitStruct);
+	
+	// read if is high
+	GPIO_PinState state = HAL_GPIO_ReadPin(pwm_port, pwm_pin);
+
+	if(state == GPIO_PIN_SET) {
+		ret = 1;
+	}
+	
+	// enable output again with prev stats
+	GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(pwm_port, &GPIO_InitStruct);
+	
+	return ret;
+}
+
+void init_fan(fan_handle_t* fan, TIM_HandleTypeDef* pwm_handle, uint32_t pwm_channel, TIM_HandleTypeDef* ic_handle, uint32_t ic_channel, uint32_t ic_active_channel, GPIO_TypeDef* pwm_port, uint16_t pwm_pin, GPIO_TypeDef* power_switch_port, uint16_t power_switch_pin) {
+	// Enable fan power
+	HAL_GPIO_WritePin(power_switch_port, power_switch_pin, GPIO_PIN_SET);
+	HAL_TIM_Base_Start_IT(pwm_handle);
+	HAL_TIM_PWM_Start(pwm_handle, pwm_channel);
+	__HAL_TIM_SET_COMPARE(pwm_handle, pwm_channel, 0);
+
+	HAL_TIM_Base_Start_IT(ic_handle);
+	HAL_TIM_IC_Start_IT(ic_handle, ic_channel);
+
+	fan->pwm_handle = pwm_handle;
+	fan->pwm_channel = pwm_channel;
+	fan->ic_handle = ic_handle;
+	fan->ic_channel = ic_channel;
+	fan->ic_channel_active = ic_active_channel;
+	fan->is_4pin = fan_detect_4pin(pwm_port, pwm_pin);
+	fan->power_switch_port = power_switch_port;
+	fan->power_switch_pin = power_switch_pin;
+
+	set_fan_pwm_percentage(fan, 25);
+}
 
 int get_fan_type(fan_handle_t* fan) {
 	if(fan->rpm == 0) {
