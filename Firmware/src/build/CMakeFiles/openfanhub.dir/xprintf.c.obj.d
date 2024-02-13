d7aaa20193a20a90333915be594860167ab253ea
Kevin <kevin@kokev.de>
2024-02-13 13:25:26 +0100

    Non crashing version

--- Firmware/src/build/CMakeFiles/openfanhub.dir/xprintf.c.obj.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/openfanhub.dir/xprintf.c.obj: \
- /mnt/hdd/home/kevin/Dokumente/Projekte/Github/OpenFanHub/Firmware/src/xprintf.c \
- /mnt/hdd/home/kevin/Dokumente/Projekte/Github/OpenFanHub/Firmware/src/xprintf.h \
- /usr/lib/gcc/arm-none-eabi/11.2.0/include/stdarg.h
--- Firmware/src/openfanhub.c
+++ Firmware/src/openfanhub.c
@@ -1,10 +1,13 @@
 
 #include "stm32f1xx.h"
 #include "STM32F1/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h"
+#include <stm32f1xx_hal.h>
 #include <stm32f1xx_hal_conf.h>
 #include <main.h>
 
 #include "STM32F1/Inc/usbd_custom_hid_if.h"
+
+#include <gpio.h>
 
 #include <stdint.h>
 #include "xprintf.h"
@@ -19,39 +22,23 @@ static uint8_t* USBD_HID_GetPos (void) {
 	return (uint8_t*)HID_Buffer;
 }
 
-void send_command(int command, const void *message)
-{
-   asm("mov r0, %[cmd];"
-       "mov r1, %[msg];"
-       "bkpt #0xAB"
-         :
-         : [cmd] "r" (command), [msg] "r" (message)
-         : "r0", "r1", "memory");
-}
+#define LED_PORT                GPIOC
+#define LED_PIN                 GPIO_PIN_13
+ #define LED_PORT_CLK_ENABLE     __HAL_RCC_GPIOC_CLK_ENABLE
 
-// dirty hack but its faster
-void serial_putc( uint8_t c ){
-	static char buf[512];
-	static uint16_t buffPos = 0;
 
-	buf[buffPos++] = c;
-
-	if(c == '\n' || buffPos >510){
-		buf[buffPos++] = 0;
-		send_command(0x04, buf);
-		buffPos = 0;
-	}
-
-}
 
 
 int main() {
 	init_cubemx();
 
+
 	while(42) {
-		HAL_Delay(200);
-		uint8_t* data = USBD_HID_GetPos();
-		USBD_CUSTOM_HID_SendReport_FS(data, 9);
+		__WFI();
+		//uint8_t* data = USBD_HID_GetPos();
+		//USBD_CUSTOM_HID_SendReport_FS(data, 9);
+	if ((HAL_GetTick() % 500) == 0)
+		 HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
 	}
 
 }
